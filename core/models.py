1a9fb23f03f9f5dc4056cb3ef9a2ce6bf20f6dec
SaemChan <scha_@naver.com>
2020-09-11 18:35:16 +0900

    4th commit

--- core/models.py
+++ core/models.py
@@ -5,8 +5,10 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(
+        auto_now_add=True
+    )  # Model이 생성된 날짜를 구하려면 auto_now_add사용
+    updated = models.DateTimeField(auto_now=True)  # 새로운 날짜로 업데이트 auto_now
 
     class Meta:
         abstract = True  # don't enter database
--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Reservation)
+class ReservatiomnAdmin(admin.ModelAdmin):
+
+    """ Revservation Admin Definition"""
+
+    pass
