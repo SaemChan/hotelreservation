9358b75c757ecb395ff493518df2ea75e263da66
SaemChan <scha_@naver.com>
2020-09-13 03:17:00 +0900

    #8

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
+from django.shortcuts import render
+
 from . import models
 
 
@@ -11,9 +13,9 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+def room_detail(request, pk):
+    print(pk)
+    return render(request, "rooms/detail.html")
